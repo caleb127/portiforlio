95b6babb2e1bd22b34d85bd75be131810a71baf2
caleb127 <matovucaleb@lbtvug.com>
2023-08-23 11:41:15 +0000

    hi

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const Terser = require('terser');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',  // Enable tree shaking by setting mode to "production"
-  entry: './src/index.js',
+  entry: './home.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   optimization: {
     minimizer: [
-      new UglifyJSPlugin({
+      new TerserPlugin({
         test: /\.js(\?.*)?$/i,  // Only minify JavaScript files
       })
     ]
